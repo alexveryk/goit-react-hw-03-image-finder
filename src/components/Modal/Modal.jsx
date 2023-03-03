eac570c71f243ec461ffd4da7dcef97643452bb1
alexveryk <veryk.av@gmail.com>
2023-03-04 00:51:32 +0200

    Create Modal.jsx

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,11 @@
+import { Overlay, ModalWindow } from './Modal.styled';
+
+export const Modal = () => {
+  return (
+    <Overlay>
+      <ModalWindow>
+        <img src="" alt="" />
+      </ModalWindow>
+    </Overlay>
+  );
+};
