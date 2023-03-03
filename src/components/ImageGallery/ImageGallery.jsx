2f33c683540e0c57d113cf9e15b5c9ad1cc87113
alexveryk <veryk.av@gmail.com>
2023-03-04 00:51:20 +0200

    Create ImageGallery.jsx

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,9 @@
+import { ImageGalleryList } from './ImageGallery.styled';
+
+export const ImageGallery = () => {
+  return (
+    <ImageGalleryList class="gallery">
+      {/* <!-- Набір <li> із зображеннями --> */}
+    </ImageGalleryList>
+  );
+};
