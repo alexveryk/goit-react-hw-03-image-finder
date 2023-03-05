bab5bd8d015e6cf9fbc08076a26e2b8fdd44dc13
alexveryk <veryk.av@gmail.com>
2023-03-05 12:15:36 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import { ContainerApp } from './App.styled';
 import { Searchbar } from './Searchbar/Searchbar';
-
-// import { Button } from './Button/Button';
+import { ToastContainer } from 'react-toastify';
+import { ImageGallery } from './ImageGallery/ImageGallery';
 
 export class App extends Component {
-  state = {};
+  state = {
+    imageName: '',
+  };
+
+  handleFormSubmit = name => {
+    // console  .log(name);
+    this.setState({ imageName: name });
+  };
 
   render() {
     return (
       <ContainerApp>
-        <Searchbar />
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        <ImageGallery imageName={this.state.imageName} />
+        <ToastContainer autoClose={2000} />
       </ContainerApp>
     );
   }
