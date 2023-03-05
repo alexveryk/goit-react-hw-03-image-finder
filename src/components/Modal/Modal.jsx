eb040f0df39f3f0543843fd80e42d3b3a3dcd033
alexveryk <veryk.av@gmail.com>
2023-03-06 00:13:14 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,40 @@
+import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
 import { Overlay, ModalWindow } from './Modal.styled';
 
-export const Modal = () => {
-  return (
-    <Overlay>
-      <ModalWindow>
-        <img src="" alt="" />
-      </ModalWindow>
-    </Overlay>
-  );
-};
+const modalRoot = document.querySelector('#modal-root');
+
+export class Modal extends Component {
+  state = {};
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = event => {
+    if (event.code === 'Escape') {
+      console.log('Close Modal');
+      this.props.onClose();
+    }
+  };
+
+  handleBackdropClick = event => {
+    if (event.currentTarget === event.target) {
+      console.log('Close Modal');
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    return createPortal(
+      <Overlay onClick={this.handleBackdropClick}>
+        {/* <ModalWindow>{this.props.children}</ModalWindow> */}
+        <ModalWindow>{<img src={this.props.children} alt="" />}</ModalWindow>
+      </Overlay>,
+      modalRoot
+    );
+  }
+}
