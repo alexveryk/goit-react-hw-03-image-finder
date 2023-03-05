50c3cc89427123b3a021cf338c7c52641ddc7f86
alexveryk <veryk.av@gmail.com>
2023-03-06 00:13:10 +0200

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,19 +1,36 @@
+import { Modal } from 'components/Modal/Modal';
+import React, { Component } from 'react';
 import { ImageItem, ImageGalleryLiImage } from './ImageGalleryItem.styled';
 
-export const ImageGalleryItem = ({ images }) => {
-  return (
-    <>
-      {images.map(image => {
-        return (
-          <ImageItem key={image.id}>
-            <ImageGalleryLiImage src={image.webformatURL} alt={image.id} />
-          </ImageItem>
-        );
-      })}
+export class ImageGalleryItem extends Component {
+  state = {
+    showModal: false,
+  };
 
-      {/* <ImageItem>
-        <ImageGalleryLiImage src="" alt="" />
-      </ImageItem> */}
-    </>
-  );
-};
+  toogleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
+
+  render() {
+    return (
+      <>
+        {this.props.images.map(image => {
+          return (
+            <>
+              <ImageItem key={image.webformatURL}>
+                <ImageGalleryLiImage
+                  src={image.webformatURL}
+                  alt={image.tags}
+                  onClick={this.toogleModal}
+                />
+              </ImageItem>
+              {this.state.showModal && (
+                <Modal onClose={this.toogleModal}>{image.webformatURL}</Modal>
+              )}
+            </>
+          );
+        })}
+      </>
+    );
+  }
+}
