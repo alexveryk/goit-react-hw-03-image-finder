684936b9d5d329cc0de61c6699a5e3e732a7ba73
alexveryk <veryk.av@gmail.com>
2023-03-05 12:15:46 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,9 +1,36 @@
+import React, { Component } from 'react';
+import { toast } from 'react-toastify';
 import { ImageGalleryList } from './ImageGallery.styled';
 
-export const ImageGallery = () => {
-  return (
-    <ImageGalleryList class="gallery">
-      {/* <!-- Набір <li> із зображеннями --> */}
-    </ImageGalleryList>
-  );
-};
+export class ImageGallery extends Component {
+  state = {
+    images: null,
+  };
+
+  async componentDidUpdate(prevProps, prevState) {
+    if (prevProps.imageName !== this.props.imageName) {
+      console.log(prevProps.imageName !== this.props.imageName);
+      console.log('prevProps: ', prevProps.imageName);
+      console.log('Curent Props', this.props.imageName);
+      fetch(
+        `https://pixabay.com/api/?key=32765009-e8a3776ebed1bf95519eebcf0&q=${this.props.imageName}`
+      ).then(response => {
+        response.json().then(images => this.setState({ images }));
+      });
+    } else {
+      const warnMessage = toast.warn(
+        'The request for this keyword has already been completed'
+      );
+      console.log('working worning');
+      return warnMessage;
+    }
+  }
+
+  render() {
+    return (
+      <ImageGalleryList>
+        {/* <!-- Набір <li> із зображеннями --> */}
+      </ImageGalleryList>
+    );
+  }
+}
