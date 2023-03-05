9c3524d182aaf98e32f2ebfe90769c955d6bf3a5
alexveryk <veryk.av@gmail.com>
2023-03-05 16:17:20 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,36 +1,77 @@
+import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import React, { Component } from 'react';
-import { toast } from 'react-toastify';
+import { ProgressBar } from 'react-loader-spinner';
 import { ImageGalleryList } from './ImageGallery.styled';
+import { Button } from 'components/Button/Button';
 
 export class ImageGallery extends Component {
   state = {
-    images: null,
+    images: [],
+    page: 1,
+    loading: false,
   };
 
-  async componentDidUpdate(prevProps, prevState) {
-    if (prevProps.imageName !== this.props.imageName) {
-      console.log(prevProps.imageName !== this.props.imageName);
-      console.log('prevProps: ', prevProps.imageName);
-      console.log('Curent Props', this.props.imageName);
+  handlePageChange = () => {
+    this.setState(prevState => {
+      return {
+        page: prevState.page + 1,
+      };
+    });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevProps.imageName !== this.props.imageName ||
+      prevState.page !== this.state.page
+    ) {
+      console.log(`name:${this.state.images.hits}`);
+      this.setState({ loading: true, image: [] });
+
       fetch(
-        `https://pixabay.com/api/?key=32765009-e8a3776ebed1bf95519eebcf0&q=${this.props.imageName}`
-      ).then(response => {
-        response.json().then(images => this.setState({ images }));
-      });
-    } else {
-      const warnMessage = toast.warn(
-        'The request for this keyword has already been completed'
-      );
-      console.log('working worning');
-      return warnMessage;
+        `https://pixabay.com/api/?key=32765009-e8a3776ebed1bf95519eebcf0&q=${this.props.imageName}&page=${this.state.page}&per_page=12`
+      )
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          return Promise.reject(new Error('Not Found'));
+        })
+        .then(images =>
+          this.setState({ images: [...this.state.images, ...images.hits] })
+        )
+        .catch(error => {
+          console.log(error);
+        })
+        .finally(this.setState({ loading: false }));
     }
   }
 
   render() {
     return (
-      <ImageGalleryList>
-        {/* <!-- Набір <li> із зображеннями --> */}
-      </ImageGalleryList>
+      <>
+        <ImageGalleryList>
+          {this.state.images.length !== 0 && (
+            <ImageGalleryItem images={this.state.images} />
+          )}
+        </ImageGalleryList>
+        {this.state.loading && (
+          <ProgressBar
+            height="80"
+            width="80"
+            ariaLabel="progress-bar-loading"
+            wrapperStyle={{
+              marginLeft: 'auto',
+              marginRight: 'auto',
+            }}
+            wrapperClass="progress-bar-wrapper"
+            borderColor="#fff"
+            barColor="#3f51b5"
+          />
+        )}
+        {this.state.images.length !== 0 && (
+          <Button onChange={this.handlePageChange} />
+        )}
+      </>
     );
   }
 }
